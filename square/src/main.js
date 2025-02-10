6ecc1ee8a8eb182cab176327d4ce5e7f3aa59f8c
jeffreytakaki <jtakaki1@gmail.com>
2025-02-10 14:51:15 -0800

    move client to main.js to prevent file  errors

--- square/src/main.js
+++ square/src/main.js
@@ -1,7 +1,12 @@
-import { squareClient } from '../square';
+import { SquareClient, SquareEnvironment } from "square";
 
 // This Appwrite function will be executed every time your function is triggered
 export default async ({ req, res, log, error }) => {
+
+  const squareClient = new SquareClient({
+    token: process.env.EXPO_PUBLIC_SQUARE_ACCESS_TOKEN, // can't leave this in here.
+    environment: SquareEnvironment.Sandbox
+  });
 
   try {
     const result = await squareClient.catalog.list();
